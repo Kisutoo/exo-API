23c28656212f01b2575c19aa72b7764d7d749889
Malkom <malk-om@outlook.fr>
2025-07-17 10:35:18 +0200

    Ajout d'une map qui focus sur la ville séléctionnée

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const selectVille = document.querySelector(".ville");
 // Séléctionne l'élément HTML avec la classe "ville"
 
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([47.5813,1.3049], 13);
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -18,13 +18,24 @@ inputCP.addEventListener("input", () => {
     // Vide le contenu actuel de la liste de sélection de ville
     selectVille.innerText = null;
     // Effectue une requête fetch vers l'API de géolocalisation avec le code postal saisi
-    fetch(`https://geo.api.gouv.fr/communes?codePostal=${value}&fields=region,nom,code,codesPostaux,codeRégion&format=json&geometry=centre`)
+    fetch(`https://geo.api.gouv.fr/communes?codePostal=${value}&fields=region,nom,code,codesPostaux,codeRégion,centre&format=json&geometry=centre`)
         // Convertit la réponse en format JSON
         .then((response) => response.json())
         // Une fois que les données JSON sont disponibles
         .then((data) => {
             // Affiche les données dans la console (pour debug si besoin)
             console.log(data);
+            
+
+            // On vient créer une option placeholder car la map ne se changera que lorsque l'on "cliquera" sur une ville. Or, en rentrant un code postal, la première ville 
+            // récupérée se mettra automatiquement dans le select, ce qui ne modifiera pas dynamiquement la map.
+            let optionPlaceHolder = document.createElement("option")
+            // On change le texte du placeHolder
+            optionPlaceHolder.innerHTML = "Séléctionner une ville";
+            // On le place en tête dans le select, avant d'afficher les villes par code postal
+            selectVille.appendChild(optionPlaceHolder);
+
+
             // Parcours chaque objet "ville" dans les données récupérées
             data.forEach((ville) => {
                 // Crée un nouvel élément d'option HTML
@@ -35,19 +46,33 @@ inputCP.addEventListener("input", () => {
                 option.innerHTML = `${ville.nom}`
                 // Ajoute l'option à la liste de séléction de ville
                 selectVille.appendChild(option);
-            })
-        })
+            }) 
+    }) 
 })
 
-selectVille.addEventListener("input", () => {
-        
-    let selectValue = selectVille.innerText;
-    console.log(selectValue); ///// Récupère tout les options: Faire en sorte de ne récupérer qu'une seule option, récupérer la longitude et la latitude, changer la map en conséquence
 
-    fetch(`https://geo.api.gouv.fr/communes?nom=${selectValue}&fields=code,nom,centre&format=json`)
+selectVille.addEventListener("change", () => {
+    // Dans le cas ou le select venait à changer via un clic sur une option
+
+    let indexVille = selectVille.selectedIndex;
+    // On récupère l'index de l'option sur laquelle on vient de cliquer
+
+    let nomVille = selectVille.children[indexVille].innerText
+    // On récupère ensuite le nom de la ville dans le select grace à l'index
+    fetch(`https://geo.api.gouv.fr/communes?nom=${nomVille}&fields=code,nom,centre&format=json&limit=1`)
+    // On effectue ensuite une autre requête fetch vers l'API de géolocalisation avec le nom de la ville cette fois ci
         .then((response) => response.json())
+        // Convertit la réponse en format JSON
         .then((data) => {
-            console.log(data)
-            // map.setView([])
+        // Une fois que les données JSON sont disponibles
+            console.log(data);
+            // Affiche les données dans la console (pour debug si besoin)
+
+            let lat = data[0].centre.coordinates[0]
+            let long = data[0].centre.coordinates[1]
+            // Récupère la longitude et la latitude de la ville pour modifier la map
+            
+            map.setView([long, lat], 15)
+            // On change ensuite la vue de la map
         })
 })
